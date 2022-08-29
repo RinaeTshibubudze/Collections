874ea1d706aca97b47c3862d770e425e64f47051
RinaeTshibubudze <vharhee@gmail.com>
2022-08-29 14:09:12 +0200

    SortedList

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -73,6 +73,54 @@ namespace Collections
 
             // clearing all data from the HashTable
             hashtbl.Clear();
+
+
+            /*
+              
+            Sorted List Code
+            */
+            var sortedList = new SortedList();
+            sortedList.Add(2, "Two Pineapples");
+            sortedList.Add(4, "Four Apples");
+            sortedList.Add(3, "Three Lemons");
+            sortedList.Add(5, "Five Mangoes");
+            sortedList.Add(1, "One Tree");
+
+            string i = (string)sortedList[1];
+            string j = (string)sortedList[5];
+            string k = (string)sortedList[3];
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+
+            //boolean value for key 5
+            bool key = sortedList.ContainsKey(5);
+
+            //boolean value for vlaue "One Tree"
+            bool val = sortedList.ContainsValue("One Tree");
+
+            //Boolean value for unavailable key
+            bool unKey = sortedList.ContainsKey(25);
+
+            //Boolean value for unavailable value
+            bool unVal = sortedList.ContainsValue("some randome value");
+            Console.WriteLine("The sorted list contains 5 key :" + key);
+            Console.WriteLine("The sorted list contains One Tree value :" + val);
+            Console.WriteLine("The sorted list contains 25 key :" + unKey);
+            Console.WriteLine("The sorted list contains some randome value:" + unVal);
+
+            //The Remove() method accepts key as argument and removes both the key and the value
+            sortedList.Remove(1);
+
+            //Now we will check if the key is present
+            bool rmvKey = sortedList.ContainsKey(1);
+            Console.WriteLine("The presence if the key is: " + rmvKey);
+
+            //The RemoveAt() method acceots index as argument and remove any key and value present at that index
+            sortedList.RemoveAt(3);
+            Console.ReadLine();
+
         }
     }
 }
